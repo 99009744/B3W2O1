01dc23ac60db658befe3722c1b7eb366a576cfd9
99009744 <55435447+99009744@users.noreply.github.com>
2020-02-18 18:30:20 +0100

    repo stond op verkeerde map
    en opdracht af (<script> deel gefixt)

---
+++ resultaat_onkunde.php
@@ -0,0 +1,33 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lacquer&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div id="fullpage">
+        
+        <h1 id="koptext">Mad Libs</h1>
+        <div id="top">
+            <a id="index.html" href="formulier_paniek.html">Er heerst paniek...</a>
+            <a id="index.html" href="formulier_onkunde.html">Onkunde</a>
+        </div>
+        <div id="bottom">
+            <h1 id="title">Er heerst paniek</h1>
+            <h3>
+                Er zijn veel mensen die niet kunnen <? echo htmlspecialchars(($_POST["vraag1"])) ?>. Neem nou <? echo htmlspecialchars(($_POST["vraag2"])) ?>. Zelf met de hulp van een <? echo htmlspecialchars(($_POST["vraag4"])) ?> of zelfs <? echo htmlspecialchars(($_POST["vraag3"])) ?> kan <? echo htmlspecialchars(($_POST["vraag2"])) ?> niet <? echo htmlspecialchars(($_POST["vraag1"])) ?>.
+                Dat heeft niets te maken met een gebrek aan <? echo htmlspecialchars(($_POST["vraag5"])) ?>, maar met een te veel aan <? echo htmlspecialchars(($_POST["vraag6"])) ?>. Te veel <? echo htmlspecialchars(($_POST["vraag6"])) ?> leidt tot <? echo htmlspecialchars(($_POST["vraag7"])) ?> en dat is niet goed als je wilt <? echo htmlspecialchars(($_POST["vraag1"])) ?>.
+                Helaas voor <? echo htmlspecialchars(($_POST["vraag2"])) ?>.
+            </h3>
+        </div>
+        <div id="footer">© Mitchell 2020</div>
+    </div>
+</body>
+</html>
---
+++ resultaat_paniek.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lacquer&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div id="fullpage">
+        
+        <h1 id="koptext">Mad Libs</h1>
+        <div id="top">
+            <a id="index.html" href="formulier_paniek.html">Er heerst paniek...</a>
+            <a id="index.html" href="formulier_onkunde.html">Onkunde</a>
+        </div>
+        <div id="bottom">
+            <h1 id="title">Er heerst paniek</h1>
+            <h3>
+                Er heerst paniek in het koningkrijk <? echo htmlspecialchars(($_POST["vraag3"])) ?>, Koning <? echo htmlspecialchars(($_POST["vraag6"])) ?> is ten einde raad en als koning <? echo htmlspecialchars(($_POST["vraag6"])) ?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <? echo htmlspecialchars(($_POST["vraag2"])) ?>.<br><br>
+                "<? echo htmlspecialchars(($_POST["vraag2"])) ?>! Het is een ramp! Het is een schandel!"<br><br>
+                "Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"<br><br>
+                "Mijn <? echo htmlspecialchars(($_POST["vraag1"])) ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <? echo htmlspecialchars(($_POST["vraag5"])) ?> voor hem gekocht!"<br><br>
+                "Majesteit, uw <? echo htmlspecialchars(($_POST["vraag1"])) ?> komt vast vanzelf weer terug?"<br><br>
+                "Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <? echo htmlspecialchars(($_POST["vraag8"])) ?> leren?"<br><br>
+                "Maar Sire, daar kunt u toch uw <? echo htmlspecialchars(($_POST["vraag7"])) ?> voor gebruiken."<br><br>
+                "<? echo htmlspecialchars(($_POST["vraag2"])) ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."<br><br>
+                "<? echo htmlspecialchars(($_POST["vraag4"])) ?>, Sire"
+            </h3>
+        </div>
+        <div id="footer">© Mitchell 2020</div>
+    </div>
+</body>
+</html>
